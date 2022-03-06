c06c06ebb90692c7ae327f9e04318ade34a0aef2
Dan Walmsley <dan@walms.co.uk>
2022-03-06 10:15:59 +0000

    add support for android.

---
+++ Wordle.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content.PM;
+using Avalonia.Android;
+
+namespace Wordle.Android
+{
+    [Activity(Label = "Wordle.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AvaloniaActivity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Content = new MainView();
+        }
+    }
+}
