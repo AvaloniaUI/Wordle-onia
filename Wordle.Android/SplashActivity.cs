aafb4963fd6dffeac77b059128872b17f3844a4d
Dan Walmsley <dan@walms.co.uk>
2022-03-07 12:58:21 +0000

    working android.

--- Wordle.Android/SplashActivity.cs
+++ Wordle.Android/SplashActivity.cs
@@ -4,27 +4,18 @@ using Android.OS;
 using Application = Android.App.Application;
 
 using Avalonia;
+using Avalonia.Android;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace Wordle.Android
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
-        protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-        }
-
         protected override void OnResume()
         {
             base.OnResume();
-
-            if (Avalonia.Application.Current == null)
-            {
-                AppBuilder.Configure<App>()
-                    .UseAndroid()
-                    .SetupWithoutStarting();
-            }
 
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
