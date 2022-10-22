cbbab55a1e90fe2e0a6b90eadf739a6ab1fc953c
Dan Walmsley <dan@walms.co.uk>
2022-10-22 02:50:07 +0100

    add wasm-browser version.

---
+++ Wordle.Browser/Program.cs
@@ -0,0 +1,16 @@
+using System.Runtime.Versioning;
+using Avalonia;
+using Avalonia.Web;
+using Wordle;
+
+[assembly:SupportedOSPlatform("browser")]
+
+internal partial class Program
+{
+    private static void Main(string[] args) =>
+        BuildAvaloniaApp()
+            .SetupBrowserApp("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>();
+}
