dabecbe1dd65b49c12fe1199ee5fc938a6f7278f
Dan Walmsley <dan@walms.co.uk>
2022-10-25 12:16:31 +0100

    remove manual ime implementation.

--- Wordle/Views/MainView.axaml.cs
+++ Wordle/Views/MainView.axaml.cs
@@ -10,73 +10,17 @@ using Wordle.Messages;
 
 namespace Wordle.Views
 {
-    internal class EntireScreenInputClient : ITextInputMethodClient
-    {
-        private InputElement? _parent;
-
-        public EntireScreenInputClient(MainView parent)
-        {
-            _parent = parent;
-        }
-
-        public void SelectInSurroundingText(int start, int end)
-        {
-            
-        }
-
-        public Rect CursorRectangle
-        {
-            get
-            {
-                return _parent.Bounds;
-            }
-        }
-
-        public event EventHandler? CursorRectangleChanged;
-        public IVisual TextViewVisual => _parent!;
-        public event EventHandler? TextViewVisualChanged;
-        public bool SupportsPreedit => false;
-        public void SetPreeditText(string text) => throw new NotSupportedException();
-
-        public bool SupportsSurroundingText => false;
-        public TextInputMethodSurroundingText SurroundingText => throw new NotSupportedException();
-        public event EventHandler? SurroundingTextChanged { add { } remove { } }
-        public string? TextBeforeCursor => null;
-        public string? TextAfterCursor => null;
-
-        private void OnCaretBoundsChanged(object? sender, EventArgs e) => CursorRectangleChanged?.Invoke(this, EventArgs.Empty);
-    }
-    
     public partial class MainView : UserControl
     {
         private readonly IMessenger _messenger;
-        private EntireScreenInputClient _imClient;
 
-        static MainView()
-        {
-            TextInputMethodClientRequestedEvent.AddClassHandler<MainView>((tb, e) =>
-            {
-                if (OperatingSystem.IsBrowser())
-                {
-                    e.Client = tb._imClient;
-                }
-            });
-        }
-        
         public MainView()
         {
             InitializeComponent();
-            
-              _imClient = new EntireScreenInputClient(this);
 
             _messenger = AvaloniaLocator.Current.GetService<IMessenger>();
             
             Dispatcher.UIThread.Post(()=>Focus(), DispatcherPriority.Loaded);
-        }
-
-        protected override void OnTextInput(TextInputEventArgs e)
-        {
-            base.OnTextInput(e);
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
